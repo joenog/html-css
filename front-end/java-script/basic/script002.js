35697afe55c5c3047212079d72d875b6338d7bec
Joe Nogueira <joabnogueira@outlook.com>
2023-01-20 16:37:24 -0300

    Repraticando Javascript

---
+++ front-end/java-script/basic/script002.js
@@ -0,0 +1,8 @@
+alert('Teste de precedencia')
+
+/*  
+O codigo abaixo tem a funação de seleciona o olemento no DOM e 
+atualizar seu valor com um calculo/number
+*/
+document.getElementById('nome').value = 'OI'
+document.querySelector('#nome').value = Number(20*2)
