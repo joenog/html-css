cef9b999f6a773272221769ad1e27a7f98a953bc
Joe Nogueira <joabnogueira@outlook.com>
2023-01-21 17:46:32 -0300

    Variaveis em js

--- front-end/java-script/basic/script002.js
+++ front-end/java-script/basic/script002.js
@@ -1,4 +1,6 @@
-alert('Teste de precedencia')
+var nome = prompt('Qual é o seu nome ?');
+var soma = Number (20*2)
+alert('Olá mundo!' + nome + soma);
 
 /*  
 O codigo abaixo tem a funação de seleciona o olemento no DOM e 
