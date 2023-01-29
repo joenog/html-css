24e238751776939849aab78f88767777d3bd07f9
Joe Nogueira <joabnogueira@outlook.com>
2023-01-29 15:49:30 -0300

    Desafio 09 Atualizado

---
+++ desafios/d09/js/playlist.js
@@ -0,0 +1,10 @@
+let playlist = document.querySelector('.playlist');
+let bnt = document.querySelector('#showhide');
+
+    function clicou() {
+        if (playlist.style.display === 'block') {
+            playlist.style.display = 'none'
+        } else {
+            playlist.style.display = 'block'
+        }
+    }  
