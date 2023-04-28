4851ba987325a2a0628522983d5c64b6e7e78fa5
Joe Nogueira <joabnogueira@outlook.com>
2023-04-27 23:16:25 -0300

    Refinando menu e adicionando animações

---
+++ encarte/js/menuFlutuante.js
@@ -0,0 +1,9 @@
+
+    let showConfigDiv = document.querySelector('#showConfig');
+    function toggleConfig() {
+    if (showConfigDiv.style.display == 'block') {
+        showConfigDiv.style.display = 'none';
+    } else {
+        showConfigDiv.style.display = 'block';
+    }
+  }
